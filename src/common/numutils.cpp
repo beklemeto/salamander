1aaffa437cbe45eae2d29a130261f8e097ea0663
beklemeto <167541833+beklemeto@users.noreply.github.com>
2025-03-21 16:36:43 +0100

    Add files via upload

---
+++ src/common/numutils.cpp
@@ -0,0 +1,34 @@
+﻿// SPDX-FileCopyrightText: 2023 Open Salamander Authors
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "precomp.h"
+
+double clampUp(double value, double up)
+{
+	return value>up? up: value;
+}
+
+double clampDown(double value, double down)
+{
+    return value < down ? down : value;
+}
+
+double clamp(double value, double down,double up)
+{
+    return value > up ? up : value < down ? down : value;
+}
+
+int clampUp(int value, int up)
+{
+    return value > up ? up : value;
+}
+
+int clampDown(int value, int down)
+{
+    return value < down ? down : value;
+}
+
+int clamp(int value, int down, int up)
+{
+    return value > up ? up : value < down ? down : value;
+}
---
+++ src/common/numutils.h
@@ -0,0 +1,22 @@
+﻿// SPDX-FileCopyrightText: 2023 Open Salamander Authors
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+//template<typename T>
+//T clampUp(T value, T up);
+//
+//template <typename T>
+//T clampDown(T value, T down);
+//
+//template <typename T>
+//T clamp(T value, T up,T down);
+
+
+double clampUp(double value, double up);
+double clampDown(double value, double down);
+double clamp(double value, double down, double up);
+
+int clampUp(int value, int up);
+int clampDown(int value, int down);
+int clamp(int value, int down, int up);
